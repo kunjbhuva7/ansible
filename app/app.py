7e733d0bcc43c13f63da205320857bb4a4b7f33f
k-bhuva <kunjbhuva.me@gmail.com>
2025-11-07 10:25:58 +0530

    ansible-7A

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello Bhuva! Flask app running successfully via GitHub Actions on port 8000."
+    return "🚀 Hello Bhuva! Flask app running successfully."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
